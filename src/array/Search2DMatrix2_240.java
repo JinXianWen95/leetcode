c4ad3e637fbd8b1cd90a176a191ee7456d5eb5ab
jinxw <jinxw@live.it>
2024-03-21 20:20:42 +0100

    improved solution for 2d array search

--- src/array/Search2DMatrix2_240.java
+++ src/array/Search2DMatrix2_240.java
@@ -1,7 +1,8 @@
 package array;
 
 public class Search2DMatrix2_240 {
-    // brute force: iterating over all numbers
+    // brute force: iterating over all numbers O(m*n)
+    // O(m+n): starting from top-right corner at each iteration we exclude one row or column
     public boolean searchMatrix(int[][] matrix, int target) {
         for (int[] row : matrix) {
             for (int n : row) {
@@ -10,6 +11,26 @@ public class Search2DMatrix2_240 {
                 }
             }
         }
+        return false;
+    }
+
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        if (matrix.length == 0) {
+            return false;
+        }
+        int row = 0;
+        int column = matrix[0].length - 1;
+        while (row < matrix.length && column >= 0) {
+            if (matrix[row][column] == target) {
+                return true;
+            }
+            if (matrix[row][column] < target) {
+                row++;
+            } else {
+                column--;
+            }
+        }
+
         return false;
     }
 }
