61c771b4988c5fe6b07f0e81f48bbf2de0e387c0
jinxw <jinxw@live.it>
2024-04-11 18:21:11 +0200

    problem 274 improved

--- src/array/HIndex_274.java
+++ src/array/HIndex_274.java
@@ -3,8 +3,7 @@ package array;
 import java.util.Arrays;
 
 public class HIndex_274 {
-    // solution 1: O(nLog(n)) for sort
-    // solution 2: O(n) if we use counting sort
+    // solution 1: O(nLog(n)) for sort with O(1) space
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
         int hIndex = 0;
@@ -14,5 +13,27 @@ public class HIndex_274 {
             }
         }
         return hIndex;
+    }
+
+    // solution 2: O(n) if we use counting sort with O(n) space
+    public int hIndex2(int[] citations) {
+        int n = citations.length;
+        int[] counts = new int[n+1];
+        for (int citation : citations) {
+            // the paper with citations more than n are counted as n
+            if (citation > n) {
+                counts[n]++;
+            } else {
+                counts[citation]++;
+            }
+        }
+        int count = 0;
+        for (int i = n; i >= 0 ; i--) {
+            count += counts[i];
+            if (count >= i) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
