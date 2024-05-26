e08d5bd8f5f026ad06a0018c25184ea22cb7cde3
jinxw <jinxw@live.it>
2024-05-26 17:14:08 +0200

    problem 122

---
+++ src/array/BestTimeToBuyAndSellStock2_122.java
@@ -0,0 +1,13 @@
+package array;
+
+public class BestTimeToBuyAndSellStock2_122 {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i+1] > prices[i]) {
+                maxProfit += prices[i+1] - prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
