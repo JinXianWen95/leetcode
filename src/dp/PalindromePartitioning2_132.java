071b0f5d00d3f572a6606dc6c67ba4f23ce75cef
jinxw <jinxw@live.it>
2024-08-04 15:12:59 +0200

    problem 132 bottom-up solution

--- src/dp/PalindromePartitioning2_132.java
+++ src/dp/PalindromePartitioning2_132.java
@@ -8,6 +8,7 @@ public class PalindromePartitioning2_132 {
     Map<String, Integer> dictionary = new HashMap<>();
     public int minCut(String s) {
         /* Top-down solution, time exceed */
+        /*
         // base case
         if (isPalindrome(s)) {
             return 0;
@@ -34,7 +35,36 @@ public class PalindromePartitioning2_132 {
             dictionary.put(s, Math.min(minCutLeft + minCutRight + 1, currentMinCut));
         }
         return dictionary.get(s);
+        */
 
+        /*
+            bottom-up solution
+         */
+        // base case
+        if (isPalindrome(s)) {
+            return 0;
+        }
+
+        int length = s.length();
+        // stores the minCut of substring of s
+        int[] minCuts = new int[length];
+        // stores if a substring [j,...., i] is a palindrome
+        boolean[][] dp = new boolean[length][length];
+
+        for (int i = 0; i < length; i++) {
+            // the max minCut of a string is its length - 1
+            int minCut = i;
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j+1 > i-1 || dp[j+1][i-1])) {
+                    dp[j][i] = true;
+                    // j == 0 because String from j to i is a palindrome, and it starts from first character so means no cuts needed
+                    // Else I need a cut at jth location, and it will be cuts encountered till j-1 + 1
+                    minCut = j == 0 ? 0 : Math.min(minCut, minCuts[j-1] + 1);
+                }
+            }
+            minCuts[i] = minCut;
+        }
+        return minCuts[length-1];
     }
 
     private boolean isPalindrome(String s) {
