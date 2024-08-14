6528a27f8c6f04446255106a03b293d3d8b2f85d
jinxw <jinxw@live.it>
2024-08-14 19:48:31 +0200

    problem 3016 improved

--- src/hashTable/MinimumPushesToTypeWord2_3016.java
+++ src/hashTable/MinimumPushesToTypeWord2_3016.java
@@ -1,10 +1,11 @@
 package hashTable;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class MinimumPushesToTypeWord2_3016 {
     public int minimumPushes(String word) {
+        // Solution 1: using map
+        /*
         final int keyNumber = 8;
         Map<Character, Integer> helper = frequencyCounter(word)
                 .entrySet().stream()
@@ -24,13 +25,43 @@ public class MinimumPushesToTypeWord2_3016 {
             }
         }
         return minPushes;
+
+         */
+
+        // Solution 2: using array
+        int[] frequencyCharacter = frequencyCounter(word);
+        Arrays.sort(frequencyCharacter);
+        int minPushes = 0;
+        int multiplyFactor = 1;
+        int counter = 0;
+        for (int i = 25; i >= 0; i--) {
+            if (frequencyCharacter[i] == 0) {
+                break;
+            }
+            minPushes += frequencyCharacter[i] * multiplyFactor;
+            counter++;
+            if (counter == 8) {
+                counter = 0;
+                multiplyFactor++;
+            }
+        }
+        return minPushes;
+
     }
 
-    private Map<Character, Integer> frequencyCounter(String word) {
-        Map<Character, Integer> result = new HashMap<>();
+//    private Map<Character, Integer> frequencyCounter(String word) {
+//        Map<Character, Integer> result = new HashMap<>();
+//        for (char c : word.toCharArray()) {
+//            result.merge(c, 1, Integer::sum);
+//        }
+//        return result;
+//    }
+
+    private int[] frequencyCounter(String word) {
+        int[] frequency = new int[26];
         for (char c : word.toCharArray()) {
-            result.merge(c, 1, Integer::sum);
+            frequency[c - 'a'] += 1;
         }
-        return result;
+        return frequency;
     }
 }
