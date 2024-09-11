9849ae800152240410b98bed1609cb3454860057
jinxianwen <jinxw@live.it>
2024-09-11 18:19:42 +0200

    daily problem 2220

---
+++ src/other/MinimumBitFlipsToConvertNumber_2220.java
@@ -0,0 +1,15 @@
+package other;
+
+public class MinimumBitFlipsToConvertNumber_2220 {
+    public int minBitFlips(int start, int goal) {
+        int xor = start ^ goal;
+
+        int result = 0;
+        while (xor > 0) {
+            result += xor & 1;
+
+            xor >>= 1;
+        }
+        return result;
+    }
+}
